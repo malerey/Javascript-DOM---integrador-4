7503c0b6b150a204ada97466886fc0631dee8a0f
Malena Rey <36973939+malerey@users.noreply.github.com>
2020-04-26 19:06:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
+// Flor, en primer lugar mil disculpas por la tardanza de esta correccion. 
+
+// 1. Tu trabajo esta fantastico en general. Cumple a la perfeccion el diseño solicitado, 
+// todo funciona como se espera y la comunicacion con la API esta muy bien lograda. 
+// En general, un trabajo excelente. 
+
+// 2. Un comentario en general es que estas escribiendo el fetch a la manera "larga" que
+// en esta API es innecesaria siempre que hagas un get
+// Quiero decir, vos escribis en general esto:
+
+// fetch(`https://tp-js-2-api-wjfqxquokl.now.sh/users`, {
+// 	method: 'GET',
+// 	headers: { 'Content-Type': 'application/json' },
+// })
+
+// pero tanto el metodo GET como esos headers son los que JavaScript pone por default, 
+// por lo que podriamos escribir esto
+
+// fetch(`https://tp-js-2-api-wjfqxquokl.now.sh/users`)
+
+// sin que cambie el comportamiento (solo en metodo GET: en POST, PUT y DELETE hay que aclarar el metodo)
+
+// 3. Como bien notaste vos, hay mucho codigo que se repite y que deberia estar en una funcion. 
+// Por ejemplo, toda la construccion de la tabla tras hacer el fetch, se repite tanto en filtrarUsuario() como en 
+// listarUsuarios(). Tambien la validacion del form en editar y agregar usuario. 
+
+// 4. Tus botones de editar y borrar tardan en arrancar porque estas llamando innecesariamente algunas funciones
+// en mostrarUsuarios() (te comente en donde), y eso hace que la pagina tarde en responder
+
+// Te deje varios comentarios en el codigo para clarificar algunas cosas, pero son todas cositas a mejorar, 
+// tu trabajo es muy bueno realmente!!
+
+
+
 const validarPhone = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '-'];
 let phoneArray = [];
 let buttonEditUser = [];
@@ -17,6 +51,16 @@ const listarUsuarios = () => {
 			let nuevoHTML = '';
 
 			data.forEach((user, i) => {
+
+				// Ojo aca, estamos usando data[i], que es una construccion que necesitamos en el for, 
+				// pero innecesaria en el forEach o map. 
+
+				// El forEach nos permite recorrer un array y a cada elemento de ese array le va a asignar
+				// el nombre que nosotras hayamos puesto en el primer parametro (en este caso, user)
+				// No es buena practica escribir data[i] en este caso, sino user
+				// El comportamiento es exactamente el mismo, y la sintaxis sera mas clara. 
+
+
 				const fullname = data[i].fullname;
 				const email = data[i].email;
 				const address = data[i].address;
@@ -44,6 +88,15 @@ const listarUsuarios = () => {
 			});
 
 			rowsData.innerHTML = nuevoHTML;
+
+							// no entiendo por que aca estas llamando a las funciones editarUsuario() y eliminarUsuario(). 
+				// No queremos que se ejecuten apenas se muestran los datos, sino solamente cuando se hace clic 
+				// en los botones. 
+				// Tal como esta ahora el codigo, se ejecutan ambas funciones apenas se cargan los usuarios, 
+				// (lo podes comprobar poniendo un console log al comienzo de esas funciones)
+				// haciendo que la API reciba dos llamadas innecesarias y que la pagina tarde un poquito mas en cargar. 
+				// Ambas llamadas deberian borrarse. 
+				// Si te quedan dudas sobre esto escribime. 
 
 			buttonEditUser = document.querySelectorAll('#editIcon');
 			editarUsuario();
@@ -114,6 +167,9 @@ const agregarUsuario = () => {
 		const formAddNewModal = formularios[0];
 
 		formAddNewModal.onsubmit = () => {
+// aca falta un e.preventDefault()
+// Fijate que si escribo por ej un nombre muy largo, 
+// el error parpadea antes de que el formulario se envie, sin mostrarmelo bien
 
 			const fullnameValue = formAddNewModal[0].value;
 			const emailValue = formAddNewModal[1].value;
@@ -122,6 +178,13 @@ const agregarUsuario = () => {
 
 			phoneArray = phoneValue.split('');
 
+			// ojo en esta validacion: estas usando un if/else
+			// eso significa que solo voy a tener un solo error, 
+			// si escribo un nombre largo y una direccion larga, 
+			// solo voy a ver el error en el nombre, 
+			// cuando deberia ver un error en ambos. 
+
+			// Deberian ser dos if sueltos. 
 			if (fullnameValue.length > 50) {
 				const fullnameLabel = document.getElementById('fullnameError');
 				fullnameLabel.innerHTML = '<p>Maximo 50 caracteres</p>';
@@ -132,6 +195,28 @@ const agregarUsuario = () => {
 				addressLabel.innerHTML = '<p>Maximo 60 caracteres</p>';
 				addressLabel.classList.add('error');
 			}
+
+			// Es buena idea la de transformar el telefono en un array para validarlo
+			// Lo que falló es que ahora tenemos dos arrays (phoneArray y validarPhone), 
+			// por lo que deberiamos hacer dos loops.
+			// Por ejemplo
+
+// else if (phoneArray.length) {
+// 			phoneArray.forEach(character => {
+// 				if (!validarPhone.includes(character)) {
+// 				const phoneLabel = document.getElementById('phoneError');
+// 				phoneLabel.innerHTML = '<p>Admite numeros, guiones y/o espacios</p>';
+// 				phoneLabel.classList.add('error');	
+// 				}
+// 			})
+// 		}
+
+// Otra cosa a tomar en cuenta es que un array, aunque este vacio, siempre se valida como true
+// Asi que si escribimos if (phoneArray) siempre vamos a entrar al if
+// Si queremos entrar solamente si el usuario escribio algo en el campo, por ejemplo, 
+// deberiamos escribir phoneArray.length 
+
+
 			// else if (phoneArray) {
 			// phoneArray.forEach(i => {
 			// 	if (validarPhone.includes(phoneArray[i]) == false) {
@@ -156,7 +241,20 @@ const agregarUsuario = () => {
 					.then(newUser => newUser.json())
 				newModal.innerHTML = ``;
 			};
-			listarUsuarios();
+			// Notaste que la lista de usuarios no se actualiza despues de agregar un usuario nuevo,
+			// a pesar de que estas llamando a esta funcion?
+			// Eso es porque el "fetch" tarda un poquito en ejercutarse (entre que se llama a la API y eso)
+			// JavaScript deja el fetch ejecutar, y sigue haciendo el resto del codigo, 
+			// que en este caso es esta funcion listarUsuarios(). 
+			// El resultado: listamos los usuarios *antes* de que se termine el fetch, y por lo tanto, 
+			// antes de que se agregue el usuario nuevo
+			// Si queremos que algo ocurra cuando termina un fetch, hay que ponerlo adentro de un .then 
+			// Y no aca afuera. 
+			// Si agregamos un segundo .then y ponemos esta funcion ahi vas a ver que la lista de usuarios se actualiza bien. 
+			// O sea, en la linea 235, apretar enter y agregar esto:
+			//  .then(data => listarUsuarios())
+
+			// listarUsuarios();
 		};
 	};
 };
@@ -171,6 +269,11 @@ buttonAddNewEmployee.onclick = () => agregarUsuario();
 
 const filtrarUsuario = (busqueda) => {
 
+	// esta muy bien este chequeo al principio, para no hacer un fetch innecesario
+	// Lo podemos mejorar asi:
+	// if (!busqueda) {
+		// tanto el string vacio como undefined valen en !busqueda asi que el funcionamiento seria el mismo, 
+		// pero el codigo seria mas claro 
 	if (busqueda == '' || busqueda == undefined) {
 		listarUsuarios();
 	} else {
@@ -184,6 +287,7 @@ const filtrarUsuario = (busqueda) => {
 				let nuevoHTML = '';
 			
 				data.forEach((user, i) => {
+					// mismo comentario al forEach que te hice antes
 					const fullname = data[i].fullname;
 					const email = data[i].email;
 					const address = data[i].address;
@@ -213,6 +317,8 @@ const filtrarUsuario = (busqueda) => {
 				rowsData.innerHTML = nuevoHTML;
 
 				buttonEditUser = document.querySelectorAll('#editIcon');
+
+// mismo comentario que antes, estas dos llamadas a las funciones son innecesarias 
 				editarUsuario(busqueda);
 				buttonDeleteUser = document.querySelectorAll('#deleteIcon');
 				eliminarUsuario(busqueda);
@@ -236,6 +342,12 @@ filterButton.onclick = () => {
 // PUT - EDITAR USUARIOS
 
 const editarUsuario = () => {
+// este primer fetch parece innecesario
+// recorda que los fetch tardan tiempo, asi que para editar un usuario
+// estamos gastando 1 o 2 segundos del tiempo del usuario
+// Y veo que la variable "data" del primer fetch no se usa en ningun lado,
+// por lo que todo el fetch podria sacarse
+// Fijate como se puede mejorar esta funcion 
 	fetch(`https://tp-js-2-api-wjfqxquokl.now.sh/users`, {
 		method: 'GET',
 		headers: { 'Content-Type': 'application/json' },
@@ -259,6 +371,12 @@ const editarUsuario = () => {
 							const email = datosUsuario.email;
 							const address = datosUsuario.address;
 							const phone = datosUsuario.phone;
+
+							// La textarea no muestra la direccion porque el elemento <textarea> de html no tiene un atributo "value"
+							// sino que debemos escribir entre la etiqueta de apertura y de cierre
+							// asi
+							// <textarea required="" name="address" id="address" cols="30" rows="10">${address}</textarea>
+
 
 							const editModal = document.getElementById('editUserModal');
 							editModal.innerHTML = `
